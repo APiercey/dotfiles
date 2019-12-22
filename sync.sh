9aa1b57ef22428f239992c22943d4d63be797397
Alexander Butt-Piercey <alexander.butt-piercey@solarisbank.de>
2019-12-22 23:52:18 +0100

    overwrite

--- sync.sh
+++ sync.sh
@@ -9,21 +9,14 @@ mkdir -p ~/.config
 (ln -sf ~/dotfiles/.config/kitty ~/.config/kitty)
 (ln -sf ~/dotfiles/.config/bat ~/.config/bat)
 
-
 # dotfiles
 (ln -sf ~/dotfiles/.zshrc ~/)
 (ln -sf ~/dotfiles/.weechat ~/)
-(ln -sf ~/dotfiles/.mutt ~/)
-(ln -sf ~/dotfiles/.notmuch-config ~/)
-(ln -sf ~/dotfiles/.neomuttrc ~/)
 (ln -sf ~/dotfiles/.tmux.conf ~/)
 (ln -sf ~/dotfiles/.taskrc ~/)
-(ln -sf ~/dotfiles/.oh-my-zsh ~/)
 (ln -sf ~/dotfiles/.asdf ~/)
 (ln -sf ~/dotfiles/.tmux ~/)
 (ln -sf ~/dotfiles/.bin ~/)
-(ln -sf ~/dotfiles/.neomuttrc ~/)
-(ln -sf ~/dotfiles/.offlineimaprc ~/)
 (ln -sf ~/dotfiles/.tool-versions ~/)
 (ln -sf ~/dotfiles/.zgen ~/)
 (ln -sf ~/dotfiles/.asdf ~/)
