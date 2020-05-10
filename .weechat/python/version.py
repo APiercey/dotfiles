ea24199cdd61166f4376b276d01ab78ff90c27e1
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-05-10 13:59:36 +0200

    Cleanup

--- .weechat/python/version.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-import weechat
-
-if weechat.register('test', 'test', 'test', 'test', 'test', 'shutdown', ''):
-    weechat.prnt('', str(sys.version))
-    weechat.prnt('', str(sys.path))
